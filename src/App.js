a63e3ce790a2a81f7de39a70b21a35ae7681920a
Hannan Ali <abdulhannanali@outlook.com>
2017-04-24 01:51:23 +0500

    add TODO

--- src/App.js
+++ src/App.js
@@ -5,40 +5,44 @@
  */
 
 import React, {Component} from 'react';
-import MainRouter from './routers'
-import {onAuthStateChanged} from './firebase/auth'
+import MainRouter from './routers';
+import {onAuthStateChanged} from './firebase/auth';
 
 export default class App extends Component {
   state = {
     authenticated: false,
     loading: true, // Assuming it's always going to load first when we start the application 
     user: undefined,
-    isAnonymous: false
+    isAnonymous: false,
   }
 
   setAuthState = (user) => {
     if (user) {
-      this.setState({ authenticated: true, loading: false, user })
+      this.setState({ authenticated: true, loading: false, user });
     } else {
-      this.setState({ loading: false })
+      this.setState({ loading: false });
     } 
   }
 
   componentDidMount() {
-    this.authListener = onAuthStateChanged(this.setAuthState)
+    this.authListener = onAuthStateChanged(this.setAuthState);
   }
 
-  componentWillUnmount () {
+  componentWillUnmount() {
     this.authListener();
   }
 
   render() {
-    const { loading, authenticated } = this.state;
+    /**
+     * TODO
+     * Add a loading state here before it changes the auth state for the first
+     * to represent an accurate state within the UI.
+     */
 
     return (
       <div>
         <MainRouter {...this.state} />
       </div>
-    )
+    );
   }
 }
